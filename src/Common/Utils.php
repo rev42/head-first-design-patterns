6161b52ca0f4a0fce1e4e150d24efac7e020aec7
Olivier Cornu <olivier.cornu@gmail.com>
2013-05-12 12:06:47 +0200

    Added common library

---
+++ src/Common/Utils.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Common;
+
+class Utils
+{
+    /**
+     * Mimic java println function
+     *
+     * @param $message
+     */
+    public static function println($message)
+    {
+        echo $message . "\n";
+    }
+}
